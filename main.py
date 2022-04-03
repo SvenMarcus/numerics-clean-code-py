ece2ed316eb9c225ee1fae0344e15602af02b1df
Sven Marcus <s.marcus@outlook.de>
2022-04-03 17:43:50 +0000

    Rename variables and add initial type hints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
 
 import numerics
@@ -6,36 +6,36 @@ import numerics
 
 L = 10.0
 W = 10.0
-dx = .25
-dy = .25
+dx = 0.25
+dy = 0.25
 ny = int(L / dy)
 nx = int(W / dx)
 
-T0 = np.ndarray((ny, nx))
-T1 = np.ndarray((ny, nx))
+T0: np.ndarray = np.ndarray((ny, nx))
+T1: np.ndarray = np.ndarray((ny, nx))
 
 nt = 1000
-dt = .1
+dt = 0.1
 
-K = .111
+K = 0.111
 
-bc = {}
+bc: dict[tuple[int, int], numerics.BoundaryCondition] = {}
 for x in range(nx):
     bc[(1, x)] = {
         "t": "d",
-        "v": 1.0
+        "v": 1.0,
     }
 
     bc[(ny - 2, x)] = {
         "t": "d",
-        "v": 0.0
+        "v": 0.0,
     }
 
 
 bc[(ny // 2, nx // 2)] = {
     "t": "n",
-    "v": -.5,
-    "d": "S"
+    "v": -0.5,
+    "d": "S",
 }
 
 
--- numerics/__init__.py
+++ numerics/__init__.py
@@ -1,44 +1,77 @@
-def ftcs(T0, T1, nt, dt, ny, dy, nx, dx, k, bc):
-    for t in range(nt):
-        for i in range(1, ny - 1):
-            for j in range(1, nx - 1):
-                if (i, j) not in bc:
-                    T1[i, j] = (
-                        T0[i, j]
+from typing import Literal, TypedDict
+import numpy as np
+
+
+class BoundaryCondition(TypedDict, total=False):
+    t: Literal["d", "n"]
+    v: float
+    d: Literal["N", "W", "S", "E"]
+
+
+def ftcs(
+    distribution: np.ndarray,
+    next_distribution: np.ndarray,
+    number_of_timesteps: int,
+    timestep_delta: float,
+    nodes_in_y: int,
+    node_distance_in_y: float,
+    nodes_in_x: int,
+    node_distance_in_x: float,
+    thermal_diffusivity: float,
+    boundary_conditions: dict[tuple[int, int], BoundaryCondition],
+):
+    for t in range(number_of_timesteps):
+        for i in range(1, nodes_in_y - 1):
+            for j in range(1, nodes_in_x - 1):
+                if (i, j) not in boundary_conditions:
+                    next_distribution[i, j] = (
+                        distribution[i, j]
                         + (
-                            (T0[i + 1, j] - 2 * T0[i, j] + T0[i - 1, j]) / (dx**2)
-                            + (T0[i, j + 1] - 2 * T0[i, j] + T0[i, j - 1]) / (dy**2)
+                            (
+                                distribution[i + 1, j]
+                                - 2 * distribution[i, j]
+                                + distribution[i - 1, j]
+                            )
+                            / (node_distance_in_x**2)
+                            + (
+                                distribution[i, j + 1]
+                                - 2 * distribution[i, j]
+                                + distribution[i, j - 1]
+                            )
+                            / (node_distance_in_y**2)
                         )
-                        * dt
-                        * k
+                        * timestep_delta
+                        * thermal_diffusivity
                     )
                 else:
-                    if bc[(i, j)]["t"] == "d":  # dirichlet
-                        T1[i, j] = bc[(i, j)]["v"]
-                    elif bc[(i, j)]["t"] == "n":  # neumann
-                        gd = bc[(i, j)]["d"]  # gradient direction
-                        v = bc[(i, j)]["v"]
+                    if boundary_conditions[(i, j)]["t"] == "d":  # dirichlet
+                        next_distribution[i, j] = boundary_conditions[(i, j)]["v"]
+                    elif boundary_conditions[(i, j)]["t"] == "n":  # neumann
+                        gd = boundary_conditions[(i, j)]["d"]  # gradient direction
+                        v = boundary_conditions[(i, j)]["v"]
                         grid_val: float
                         sign: int
                         grid_distance: float
                         if gd == "N":
-                            grid_val = T0[i - 2, j]
+                            grid_val = distribution[i - 2, j]
                             sign = 1
-                            grid_distance = dy
+                            grid_distance = node_distance_in_y
                         elif gd == "S":
-                            grid_val = T0[i + 2, j]
+                            grid_val = distribution[i + 2, j]
                             sign = -1
-                            grid_distance = dy
+                            grid_distance = node_distance_in_y
                         elif gd == "W":
-                            grid_val = T0[i, j - 2]
+                            grid_val = distribution[i, j - 2]
                             sign = 1
-                            grid_distance = dx
+                            grid_distance = node_distance_in_x
                         elif gd == "E":
-                            grid_val = T0[i, j + 2]
+                            grid_val = distribution[i, j + 2]
                             sign = -1
-                            grid_distance = dx
+                            grid_distance = node_distance_in_x
 
-                        T1[i, j] = grid_val + 2 * sign * v * grid_distance
-        T0, T1 = T1, T0
+                        next_distribution[i, j] = (
+                            grid_val + 2 * sign * v * grid_distance
+                        )
+        distribution, next_distribution = next_distribution, distribution
 
-    return T0
+    return distribution
