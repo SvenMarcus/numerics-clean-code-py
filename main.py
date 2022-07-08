7a93d32f05646731a1afd4e30f10dbf369e734d9
Sven Marcus <s.marcus@outlook.de>
2022-07-08 16:22:27 +0200

    Use optimized simulation

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import numerics
 from numerics.animate import animate
 
 
-L = 10.0
-W = 10.0
+L = 50.0
+W = 50.0
 dx = 0.25
 dy = 0.25
 ny = int(L / dy)
@@ -19,13 +19,21 @@ dt = 0.1
 
 K = 0.111
 
-bc = {}
-for x in range(nx):
-    bc[(1, x)] = {"t": "d", "v": 1.0}
+# BC Explanations
+# t: Type           d or n
+# p: posistions     tuple of slices (y, x)
+# v: value          value of bc
+# d: direction      direction of bc (only neumann)
 
-    bc[(ny - 2, x)] = {"t": "d", "v": 0.0}
-
-
-bc[(ny // 2, nx // 2)] = {"t": "n", "v": -0.5, "d": "S"}
+bc = []
+bc.append({"t": "d", "p": (slice(1, 2), slice(nx)), "v": 1.0})
+bc.append(
+    {
+        "t": "n",
+        "p": (ny // 2, nx // 2),
+        "v": 0.5,
+        "d": "S",
+    }
+)
 
 animate(lambda nt: numerics.ftcs(T0, T1, nt, dt, ny, dy, nx, dy, K, bc))
--- numerics/__init__.py
+++ numerics/__init__.py
@@ -1,43 +1,36 @@
 def ftcs(T0, T1, nt, dt, ny, dy, nx, dx, k, bc):
     for t in range(nt):
-        for i in range(1, ny - 1):
-            for j in range(1, nx - 1):
-                if (i, j) not in bc:
-                    T1[i, j] = (
-                        T0[i, j]
-                        + (
-                            (T0[i + 1, j] - 2 * T0[i, j] + T0[i - 1, j]) / (dx**2)
-                            + (T0[i, j + 1] - 2 * T0[i, j] + T0[i, j - 1]) / (dy**2)
-                        )
-                        * dt
-                        * k
-                    )
-                else:
-                    if bc[(i, j)]["t"] == "d":  # dirichlet
-                        T1[i, j] = bc[(i, j)]["v"]
-                    elif bc[(i, j)]["t"] == "n":  # neumann
-                        gd = bc[(i, j)]["d"]  # gradient direction
-                        v = bc[(i, j)]["v"]
-                        val: float
-                        dist: float
-                        if gd == "N":
-                            val = T0[i - 2, j]
-                            s = 1
-                            dist = dy
-                        elif gd == "S":
-                            val = T0[i + 2, j]
-                            s = -1
-                            dist = dy
-                        elif gd == "W":
-                            val = T0[i, j - 2]
-                            s = 1
-                            dist = dx
-                        elif gd == "E":
-                            val = T0[i, j + 2]
-                            s = -1
-                            dist = dx
+        T1[1:-1, 1:-1] = T0[1:-1, 1:-1] + k * dt * (
+            (T0[2:, 1:-1] - 2 * T0[1:-1, 1:-1] + T0[:-2, 1:-1]) / (dx**2)
+            + (T0[1:-1, 2:] - 2 * T0[1:-1, 1:-1] + T0[1:-1, :-2]) / (dy**2)
+        )
+        for _bc in bc:
+            i, j = _bc["p"]
+            if _bc["t"] == "d":  # dirichlet
+                T1[i, j] = _bc["v"]
+            elif _bc["t"] == "n":  # neumann
+                gd = _bc["d"]  # gradient direction
+                v = _bc["v"]
+                val: float
+                dist: float
+                if gd == "N":
+                    val = T0[i - 2, j]
+                    s = 1
+                    dist = dy
+                elif gd == "S":
+                    val = T0[i + 2, j]
+                    s = -1
+                    dist = dy
+                elif gd == "W":
+                    val = T0[i, j - 2]
+                    s = 1
+                    dist = dx
+                elif gd == "E":
+                    val = T0[i, j + 2]
+                    s = -1
+                    dist = dx
 
-                        T1[i, j] = val + 2 * s * v * dist
+                T1[i, j] = val + 2 * s * v * dist
 
         T0, T1 = T1, T0
 
